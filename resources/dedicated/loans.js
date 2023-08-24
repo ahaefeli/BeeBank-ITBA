f8498eab7082a835acf02c15057aaa14af02c6ca
ahaefeli <agus.haefeli@gmail.com>
2023-08-24 17:48:34 -0300

    added loans.html

---
+++ resources/dedicated/loans.js
@@ -0,0 +1,12 @@
+
+const acpbtn = document.querySelector(".acept-btn")
+const cancel = document.querySelector(".cancel-btn")
+
+acpbtn.addEventListener("click", () => {
+    confirm("Esta seguro que quiere confirmar este prestamo en BEEBANK")
+});
+
+cancel.addEventListener("click", () => {
+    confirm("Esta seguro que quiere desechar este prestamo en BEEBANK")
+});
+
