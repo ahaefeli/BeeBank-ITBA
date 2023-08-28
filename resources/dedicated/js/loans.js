59f33308a28cfcc9b9bb304d659cb1cc95edce40
ahaefeli <agus.haefeli@gmail.com>
2023-08-28 03:08:47 -0300

    minor fixes

--- resources/dedicated/js/loans.js
+++ resources/dedicated/js/loans.js
@@ -1,11 +1,11 @@
 
-const acpbtn = document.querySelector(".acept-btn")
-const cancel = document.querySelector(".cancel-btn")
+const acpbtn = document.querySelector('[name="accept-btn"]')
+const cancel = document.querySelector('[name="cancel-btn"]')
 
 acpbtn.addEventListener("click", () => {
-    confirm("Esta seguro que quiere confirmar este prestamo en BEEBANK")
+    confirm("¿Deseea confirmar el préstamo de BeeBank?")
 });
 
 cancel.addEventListener("click", () => {
-    confirm("Esta seguro que quiere desechar este prestamo en BEEBANK")
+    confirm("¿Está seguro que quiere descartar el prestamo?")
 });
