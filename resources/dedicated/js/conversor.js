48558962b2cb9a708a52e9a0b5c71c7bbf3e6676
ahaefeli <agus.haefeli@gmail.com>
2023-08-29 18:35:20 -0300

    added config and minor fixes
    font import is now from the main css

--- resources/dedicated/js/conversor.js
+++ resources/dedicated/js/conversor.js
@@ -1,44 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById("convert").addEventListener("click", function() {
+    const convertButton = document.getElementById("convert")
+
+    convertButton.addEventListener("click", function () {
 
         const currencySelect = document.getElementById("currency");
         const selectedCurrency = currencySelect.value;
 
         const amountInput = document.getElementById("amount");
         const amount = parseFloat(amountInput.value);
-      
+
         if (isNaN(amount) || amount < 0) {
             amount = 0;
             amountInput.value = "0";
         }
-     
+
         const ARS = document.getElementById("ARS");
         const USD = document.getElementById("USD");
         const EUR = document.getElementById("EUR");
         const UYU = document.getElementById("UYU");
-        
-        if (selectedCurrency == "ARS") {
-            ARS.textContent = "$"+(amount).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            USD.textContent = "$"+(amount*0.0029).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            EUR.textContent = "$"+(amount*0.0026).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            UYU.textContent = "$"+(amount*0.11).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-        } else if (selectedCurrency == "USD"){
-            ARS.textContent = "$"+(amount*350.58).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            USD.textContent = "$"+(amount).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            EUR.textContent = "$"+(amount*0.92).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            UYU.textContent = "$"+(amount*37.69).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-        } else if (selectedCurrency == "EUR"){
-            ARS.textContent = "$"+(amount*382.5).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            USD.textContent = "$"+(amount*1.08).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            EUR.textContent = "$"+(amount).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            UYU.textContent = "$"+(amount*40.83).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-        } else if (selectedCurrency == "UYU"){
-            ARS.textContent = "$"+(amount*9.28).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            USD.textContent = "$"+(amount*0.027).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            EUR.textContent = "$"+(amount*0.024).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-            UYU.textContent = "$"+(amount).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 8});
-        }
-        });
 
+        // TODO: Connect with API
+        const getValue = (amount, price) => (amount * price).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 8 });
+
+
+        if (selectedCurrency == "ARS") {
+            ARS.textContent = "$" + getValue(amount, 1);
+            USD.textContent = "$" + getValue(amount, 0.0029);
+            EUR.textContent = "$" + getValue(amount, 0.0026);
+            UYU.textContent = "$" + getValue(amount, 0.11);
+        } else if (selectedCurrency == "USD") {
+            ARS.textContent = "$" + getValue(amount, 350.58);
+            USD.textContent = "$" + getValue(amount, 1);
+            EUR.textContent = "$" + getValue(amount, 0.92);
+            UYU.textContent = "$" + getValue(amount, 37.69);
+        } else if (selectedCurrency == "EUR") {
+            ARS.textContent = "$" + getValue(amount, 382.5);
+            USD.textContent = "$" + getValue(amount, 1.08);
+            EUR.textContent = "$" + getValue(amount, 1);
+            UYU.textContent = "$" + getValue(amount, 40.83);
+        } else if (selectedCurrency == "UYU") {
+            ARS.textContent = "$" + getValue(amount, 9.28);
+            USD.textContent = "$" + getValue(amount, 0.027);
+            EUR.textContent = "$" + getValue(amount, 0.024);
+            UYU.textContent = "$" + getValue(amount, 1);
+        }
     });
+
+    // Exec the conversion when Enter is pressed
+    const currencySelect = document.getElementById("amount");
+    currencySelect.addEventListener("keyup", function (event) {
+            if (event.key == "Enter") {
+            convertButton.click();
+        };
+        
+    });
+
+});
