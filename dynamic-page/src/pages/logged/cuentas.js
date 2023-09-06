f13e5bef6a1f20e22e0e362f4504d447c66e9745
ahaefeli <agus.haefeli@gmail.com>
2023-09-06 13:48:20 -0300

    added accounts page

--- dynamic-page/src/pages/logged/cuentas.js
+++ dynamic-page/src/pages/logged/cuentas.js
@@ -1,10 +1,19 @@
-import LoggedNavBar from '../../components/loggedNavBar';
+  import { useEffect, useState } from 'react';
+  import LoggedNavBar from '../../components/loggedNavBar';
 import LoggedAccountsContent from '../../components/logged/accounts-content';
 import Logo from '../../components/logged/logo'
+import accountsFunctions from '../../js/accounts-functionality';
 
 import '../css/dedicated/accounts-content.css';
 
 export default function Cuentas() {
+    let [executeEffect, setExecutEffect] = useState(true);
+    useEffect(() => {
+        if (executeEffect) {
+          accountsFunctions();
+        setExecutEffect = false;  }
+    }, [executeEffect]);
+    
   return (
     <div className='ReactContent'>
       <Logo mobile={true}/>
