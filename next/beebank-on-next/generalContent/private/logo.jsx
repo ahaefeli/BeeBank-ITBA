5e67ac469cafc098b6184e691c45d07c1697c9e9
ahaefeli <agus.haefeli@gmail.com>
2023-09-20 04:28:48 -0300

    fixed logo link

--- next/beebank-on-next/generalContent/private/logo.jsx
+++ next/beebank-on-next/generalContent/private/logo.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function Logo(props) {
   return (
     <div className={props.mobile ? "logo_mobile" : "logo"}>
-        <Link href="/home"><img src={props.darkLogo?'https://raw.githubusercontent.com/ahaefeli/BeeBank-ITBA/main/src/resources/beebank-logo-oscuro.png':'https://raw.githubusercontent.com/ahaefeli/BeeBank-ITBA/main/src/resources/beebank-logo-claro.png'} className="bbank_logo" alt="Beebank" draggable="false" /></Link>
+        <Link href="/home"><Image width={200} height={50} quality={90} src={`https://raw.githubusercontent.com/ahaefeli/beebank-resources/main/beebank-logo-${props.darkLogo?'oscuro':'claro'}.png`} className="bbank_logo" alt="Beebank" draggable="false" /></Link>
     </div>
   )
 }
