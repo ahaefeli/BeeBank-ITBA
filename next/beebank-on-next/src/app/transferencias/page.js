17f12e9db205857f0846efdba163ae36a3839302
LMartinDeAmeller <deamellertv@gmail.com>
2023-09-20 00:01:07 -0300

    Agregado de dinamismo v1
    Se agrego dinamismo y automatizacion a las paginas de transferencias

--- next/beebank-on-next/src/app/transferencias/page.js
+++ next/beebank-on-next/src/app/transferencias/page.js
@@ -1,15 +1,19 @@
+"use client"
 import PrivateNavBar from '../../../generalContent/private/privateNavBar';
 import TransfersContent from './transfersContent';
 import Logo from '../../../generalContent/private/logo'
 import styleTransfers from './transfers.module.css'
+//import CFactura from "./CompFactura";
 
 export default function Home() {
+
+//<CFactura {...facturaData}/>
   return (
     <div className='ReactContent'>
       <Logo mobile={true} darkLogo={true}/>
       <PrivateNavBar/>
       <Logo mobile={false} darkLogo={true}/>
-      <TransfersContent />
+      <TransfersContent/>
     </div>
   );
 }
