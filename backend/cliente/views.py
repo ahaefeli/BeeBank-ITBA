6b9d4fe7ad4477cfbd0ce618cf2e172b9867bc34
LMartinDeAmeller <deamellertv@gmail.com>
2023-12-04 16:27:44 -0300

    Agregado de pagination
    Agregado de pagination y url especifica

--- backend/cliente/views.py
+++ backend/cliente/views.py
@@ -1,15 +1,23 @@
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response 
 from .serializer import UserSerializer, DireccionSerializer
 from django.contrib.auth.models import User
 from .models import Direccion
-from rest_framework.views import APIView
 
 # 127.0.0.1/cliente/api/users/
 class UserList(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get(self, request):
+        usuarios = User.objects.all()
+        paginator = LimitOffsetPagination()
+        result_page = paginator.paginate_queryset(usuarios, request)
+        serializer = UserSerializer(result_page, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class UserListDetail(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticated]
--- backend/webProject/settings.py
+++ backend/webProject/settings.py
@@ -107,6 +107,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': None
 }
 
 LOGIN_URL = "/admin/"
