71b6c0312c7a45e5eefa050cf2ce17c3ffd73add
LMartinDeAmeller <deamellertv@gmail.com>
2023-12-07 04:55:27 -0300

    agregado de lista de sucursales de staff
    .

--- frontend/src/app/home/staffView/staffView.jsx
+++ frontend/src/app/home/staffView/staffView.jsx
@@ -1,4 +1,6 @@
 'use client'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import staffViewStyle from './staffView.module.css'
 //componente para staffs
@@ -6,20 +8,91 @@ import Cookies from "js-cookie"
 
 export default function StaffView(){
     const cIsStaff = Cookies.get("cIsStaff") == "true"? true: false
+    const [view, setView] = useState("MainMenu")
+
+    const [LiItemsSucursales, setLiItemsSucursales] = useState()
+
+    function ChangeView(window){
+        setView(window)
+    }
+
+    useEffect(()=>{
+        if(view=="ListadoSucursales"){
+            let LiItemSucursales = []
+            axios.get("http://localhost:8000/sucursal/show/",{
+            auth:{
+                username:'admin',
+                password:'admin'
+            }
+            }).then((response)=>{
+                response.data.forEach((sucursal)=>{
+                    LiItemSucursales.push(
+                        <div>
+                            <p>Sucursal: {sucursal.branch_name}</p>
+                            <p>Direccion: {sucursal.direccion.street} {sucursal.direccion.address_number}</p>
+                            <p>-------------------------------------------------------------------------------</p>
+                        </div>
+                    )
+                })
+            setLiItemsSucursales(LiItemSucursales)
+        })
+        }
+        
+        
+    },[view])
 
     if(cIsStaff){
-        return(
-            <div>
-                STAFF MODE
-                <div className={staffViewStyle.Menu}>
-                    <button className='button--general'>PRESTAMOS DE SUCURSALES</button>
-                    <button className='button--general'>TARJETAS ASOCIADAS</button>
-                    <button className='button--general'>SOLICITUD PRESTAMO</button>
-                    <button className='button--general'>MODIFICAR DIRECCION CLIENTE</button>
-                    <button className='button--general'>LISTADO SUCURSALES</button>
+        if(view=="MainMenu"){
+            return(
+                <div>
+                    STAFF MODE
+                    <div className={staffViewStyle.Menu}>
+                        <button className='button--general' onClick={()=>ChangeView("PrestamosSucursales")}>PRESTAMOS DE SUCURSALES</button>
+                        <button className='button--general' onClick={()=>ChangeView("TarjetasAsociadas")}>TARJETAS ASOCIADAS</button>
+                        <button className='button--general' onClick={()=>ChangeView("SolicitudPrestamo")}>SOLICITUD PRESTAMO</button>
+                        <button className='button--general' onClick={()=>ChangeView("ModificarDireccion")}>MODIFICAR DIRECCION CLIENTE</button>
+                        <button className='button--general' onClick={()=>ChangeView("ListadoSucursales")}>LISTADO SUCURSALES</button>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
+        else if(view=="PrestamosSucursales"){
+            return(
+                <div>
+                    
+                </div>
+            )
+        }
+        else if(view=="TarjetasAsociadas"){
+            return(
+                <div>
+                    1
+                </div>
+            )
+        }
+        else if(view=="SolicitudPrestamo"){
+            return(
+                <div>
+                    2
+                </div>
+            )
+        }
+        else if(view=="ModificarDireccion"){
+            return(
+                <div>
+                    3
+                </div>
+            )
+        }
+        else if(view=="ListadoSucursales"){
+
+            return(
+                <div>
+                    {LiItemsSucursales}
+                </div>
+            )
+        }
+
     }
     else{
         return(<></>)
